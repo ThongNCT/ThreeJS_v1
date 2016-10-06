9c47ac0d00fbd046b4320fc5541e4e5eddab4d2d
chithong1612 <chithong1612@gmail.com>
2016-10-06 07:58:37 +0700

    Init code v1

---
+++ ThreeJS.Web/Gruntfile.js
@@ -0,0 +1,88 @@
+﻿/*
+This file in the main entry point for defining grunt tasks and using grunt plugins.
+Click here to learn more. http://go.microsoft.com/fwlink/?LinkID=513275&clcid=0x409
+*/
+module.exports = function (grunt) {
+
+    // Load npm tasks
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-ts');
+
+    grunt.initConfig({
+        ts: {
+            base: {
+                //src: ['abc'],
+                src: ['Scripts/app/boot.ts', 'Scripts/app/**/*.ts'],
+                outDir: 'wwwroot/app',
+                tsconfig: './tsconfig.json'
+            }
+        },
+        concat: {
+            options: {
+                separator: '\r\n;\r\n',
+            },
+            appScripts: {
+                src: ['wwwroot/app/**/*.js'],
+                dest: 'wwwroot/threejs-all.js',
+            }
+        },
+        uglify: {
+            my_target: {
+                files: [{
+                    expand: true,
+                    //cwd: 'wwwroot/app',
+                    src: [
+                        //'**/*.js'
+                    ],
+                    dest: 'wwwroot/app'
+                }]
+            },
+            options: {
+                sourceMap: true
+            }
+        },
+
+        // Copy all JS files from external libraries and required NPM packages to wwwroot/js
+        copy: {
+            main: {
+                files: [
+                {
+                    expand: true,
+                    flatten: true,
+                    src: [
+                        //'Script/app/**/*.js'
+                    ],
+                    dest: 'wwwroot/app/',
+                    filter: 'isFile'
+                },
+                {
+                    expand: true,
+                    flatten: true,
+                    src: [
+                        'node_modules/**/*.js'
+                    ],
+                    dest: 'wwwroot/lib/',
+                    filter: 'isFile'
+                }]
+            }
+        },
+
+        // Watch specified files and define what to do upon file changes
+        watch: {
+            scripts: {
+                files: ['Scripts/**/*.js'],
+                tasks: ['ts', 'uglify', 'copy']
+            }
+        }
+    });
+
+    // Define the default task so it will launch all other tasks
+    // Compile ts files and concat into script
+    grunt.registerTask('default', ['ts', 'concat']);
+    //grunt.registerTask('compile', ['ts']);
+    //grunt.registerTask('combine', ['concat']);
+    //grunt.registerTask('uglifyscript', ['ts', 'uglify', 'copy']);
+};
---
+++ ThreeJS.Web/Readme.txt
@@ -0,0 +1,9 @@
+- Install NodeJs
+- Open command line in folder of project web 
+- Run command: npm install
+
+- Packages.json: identifies npm package dependencies for the project.
+- tsconfig.json defines how the TypeScript compiler generates JavaScript from the project's files.
+- typings.json provides additional definition files for libraries that the TypeScript compiler doesn't natively recognize.
+- systemjs.config.js provides information to a module loader about where to find application modules, and registers all the necessary packages. 
+It also contains other packages that will be needed by later documentation examples.
